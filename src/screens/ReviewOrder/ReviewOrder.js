8d151ed3671f1b11a5c7219071665acfa0d156ca
Mabel Lawrence <mabelmail3@gmail.com>
2021-07-07 10:54:34 -0400

    refactored to class component

--- src/screens/ReviewOrder/ReviewOrder.js
+++ src/screens/ReviewOrder/ReviewOrder.js
@@ -10,20 +10,58 @@ import {
   TextInput,
 } from 'react-native';
 
-//PLACEHOLDER COMPONENT
+export default class ReviewOrder extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      food: [
+        { name: 'carrot', expiration: new Date() },
+        { name: 'tomato', expiration: new Date() },
+      ],
+      modalVisible: false,
+    };
+    this.setModalVisible = this.setModalVisible.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onAddItem = this.onAddItem.bind(this);
+  }
 
-export default function ReviewOrder({ navigation }) {
-  //set modal visibility
-  const [modalVisible, setModalVisible] = useState(false);
-
-  const onSubmit = () => {
+  onSubmit = () => {
     navigation.navigate('Scanner');
   };
 
-  const onAddItem = () => {
+  onAddItem = () => {
     setModalVisible(!modalVisible);
   };
-  const maybeRenderModal = () => {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Review Your Order</Text>
+        <Text style={styles.title}>Order Date: {new Date()}</Text>
+        <table>
+          <tr>
+            <th>Item</th>
+            <th>Expiration</th>
+          </tr>
+          {this.state.food.map((item) => {
+            return (
+              <tr key={item.name}>
+                <td>{item.name}</td>
+                <td>{item.expiration}</td>
+              </tr>
+            );
+          })}
+        </table>
+        <TouchableOpacity style={styles.button} onPress={() => onAddItem()}>
+          <Text style={styles.buttonTitle}>AddItem</Text>
+        </TouchableOpacity>
+        {maybeRenderModal()}
+      </View>
+    );
+  }
+
+  //modal component
+  maybeRenderModal = () => {
     return (
       <Modal
         animationType="none"
@@ -64,14 +102,70 @@ export default function ReviewOrder({ navigation }) {
       </Modal>
     );
   };
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Review Your Order</Text>
-      <TouchableOpacity style={styles.button} onPress={() => onAddItem()}>
-        <Text style={styles.buttonTitle}>AddItem</Text>
-      </TouchableOpacity>
-      {maybeRenderModal()}
-    </View>
-  );
 }
+
+//functional component
+
+// export default function ReviewOrder({ navigation }) {
+//   //set modal visibility
+//   const [modalVisible, setModalVisible] = useState(false);
+
+//   const onSubmit = () => {
+//     navigation.navigate('Scanner');
+//   };
+
+//   const onAddItem = () => {
+//     setModalVisible(!modalVisible);
+//   };
+//   const maybeRenderModal = () => {
+//     return (
+//       <Modal
+//         animationType="none"
+//         transparent={true}
+//         visible={modalVisible}
+//         onRequestClose={() => {
+//           Alert.alert('Modal has been closed.');
+//           setModalVisible(!modalVisible);
+//         }}
+//       >
+//         <View style={styles.modalView}>
+//           <Text style={styles.modalText}>Add an Item</Text>
+//           <TextInput
+//             style={styles.input}
+//             placeholder="Food"
+//             placeholderTextColor="#aaaaaa"
+//             // onChangeText={(text) => setFood(text)}
+//             // value={email}
+//             underlineColorAndroid="transparent"
+//             autoCapitalize="none"
+//           />
+//           <TextInput
+//             style={styles.input}
+//             placeholder="Expiration Date"
+//             placeholderTextColor="#aaaaaa"
+//             // onChangeText={(text) => setExpiration(text)}
+//             // value={email}
+//             underlineColorAndroid="transparent"
+//             autoCapitalize="none"
+//           />
+//           <Pressable
+//             style={[styles.button, styles.buttonClose]}
+//             onPress={() => setModalVisible(!modalVisible)}
+//           >
+//             <Text style={styles.textStyle}>Hide Modal</Text>
+//           </Pressable>
+//         </View>
+//       </Modal>
+//     );
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.title}>Review Your Order</Text>
+//       <TouchableOpacity style={styles.button} onPress={() => onAddItem()}>
+//         <Text style={styles.buttonTitle}>AddItem</Text>
+//       </TouchableOpacity>
+//       {maybeRenderModal()}
+//     </View>
+//   );
+// }
