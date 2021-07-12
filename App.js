2f7d4c03d05f91dc5ea2efa4d081849fbc44d03e
Laura Buffington <laura.c.buffington@gmail.com>
2021-07-12 12:44:27 -0700

    remove dummy import, comments

--- App.js
+++ App.js
@@ -21,17 +21,9 @@ import store from './src/store';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-// import DummyNotification from './src/screens/ReusableComponents/DummyNotification';
-// import Constants from 'expo-constants';
-// import Ionicons from 'react-native-vector-icons/Ionicons'
-
-// const store = configureStore();
-import DummyNotification from './src/screens/ReusableComponents/DummyNotification';
 import Constants from 'expo-constants';
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import  FridgeNavigator  from './src/screens/FridgeScreen/FridgeNavigator';
-
-// const store = configureStore();
 
 if (!global.btoa) {
   global.btoa = encode;
