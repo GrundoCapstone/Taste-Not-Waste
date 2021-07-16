2d300bf0abbe88633ea4d33692cb2f7d306e5d30
Mabel Lawrence <mabelmail3@gmail.com>
2021-07-16 15:00:49 -0400

    added borders around login and registration buttons

--- src/screens/LoginScreen/styles.js
+++ src/screens/LoginScreen/styles.js
@@ -1,64 +1,66 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        backgroundColor: '#EAC4D5'
-    },
-    logo: {
-        flex: 1,
-        height: 120,
-        width: 90,
-        alignSelf: "center",
-        margin: 0,
-    },
-    input: {
-        height: 48,
-        borderRadius: 5,
-        overflow: 'hidden',
-        backgroundColor: 'white',
-        marginTop: 10,
-        marginBottom: 10,
-        marginLeft: 30,
-        marginRight: 30,
-        paddingLeft: 16,
-        fontFamily: 'Kalam',
-    },
-    button: {
-        backgroundColor: '#B8E0D2',
-        marginLeft: 30,
-        marginRight: 30,
-        marginTop: 20,
-        height: 48,
-        borderRadius: 5,
-        alignItems: "center",
-        justifyContent: 'center'
-    },
-    buttonTitle: {
-        color: 'black',
-        fontSize: 16,
-        fontWeight: "bold",
-        fontFamily: 'Kalam',
-    },
-    footerView: {
-        flex: 1,
-        alignItems: "center",
-        marginTop: 20
-    },
-    footerText: {
-        fontSize: 16,
-        color: '#2e2e2d'
-    },
-    footerLink: {
-        color: "#788eec",
-        fontWeight: "bold",
-        fontSize: 16
-    },
-    title: {
-        marginTop: 60,
-        textAlign: 'center',
-        fontFamily: 'Kalam',
-        fontSize: 30
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#EAC4D5',
+  },
+  logo: {
+    flex: 1,
+    height: 120,
+    width: 90,
+    alignSelf: 'center',
+    margin: 0,
+  },
+  input: {
+    height: 48,
+    borderRadius: 5,
+    overflow: 'hidden',
+    backgroundColor: 'white',
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 30,
+    marginRight: 30,
+    paddingLeft: 16,
+    fontFamily: 'Kalam',
+  },
+  button: {
+    backgroundColor: '#B8E0D2',
+    marginLeft: 30,
+    marginRight: 30,
+    marginTop: 20,
+    height: 48,
+    borderRadius: 5,
+    borderColor: 'black',
+    borderWidth: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonTitle: {
+    color: 'black',
+    fontSize: 16,
+    fontWeight: 'bold',
+    fontFamily: 'Kalam',
+  },
+  footerView: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  footerText: {
+    fontSize: 16,
+    color: '#2e2e2d',
+  },
+  footerLink: {
+    color: '#788eec',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  title: {
+    marginTop: 60,
+    textAlign: 'center',
+    fontFamily: 'Kalam',
+    fontSize: 30,
+  },
+});
